7fb981cd8da1abc8ea3cfb6d160490007ed86c0b
muhudinsalat <muhudinsalat80@gmail.com>
2025-12-18 07:55:12 +0100

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+   
+  let form = document.getElementById("create-task-form");
+
+ 
+  form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    
+    let input = document.getElementById("new-task-description");
+
+
+    let li = document.createElement("li");
+
+    li.textContent = input.value;
+
+   
+    let taskList = document.getElementById("tasks");
+
+    
+    taskList.appendChild(li);
+
+    input.value = "";
+  });
 });
